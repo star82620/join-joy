84a6b9a460d90a09902c4d1e117f58cdcad28a85
Ayu Huang <star82620@gmail.com>
2023-10-12 22:54:31 +0800

    update/ adjust folder & link

--- common/components/GeneralLink.tsx
+++ common/components/GeneralLink.tsx
@@ -2,7 +2,6 @@ import React, { Children, ReactNode } from "react";
 import Link from "next/link";
 
 // 執行此元件 function：給予對應的 props（url、target），輸出結果為 內部連結<Link> 或 外部連結<a>
-// 如果有 target 就是外部連結，不然就是 Link
 
 interface Props {
   href: string;
@@ -11,10 +10,22 @@ interface Props {
 }
 
 export default function GeneralLink({ href, children, target }: Props) {
-  if (target) {
+  // 如果沒有 href => 什麼都不給 return null
+  if (!href) return null;
+
+  // 如果有 href 但沒有 target => 內部連結
+  if (href && !target)
+    return (
+      <Link className="bg-red-200" href={href}>
+        {children}
+      </Link>
+    );
+
+  // 如果有 href 和 target => 外部連結
+  if (href && target)
     return (
       <a
-        className=" bg-yellow-400"
+        className="bg-yellow-400"
         href={href}
         target={target}
         rel="noreferrer noopener"
@@ -22,11 +33,4 @@ export default function GeneralLink({ href, children, target }: Props) {
         {children}
       </a>
     );
-  } else {
-    return (
-      <Link className=" bg-red-200" href={href}>
-        {children}
-      </Link>
-    );
-  }
 }
