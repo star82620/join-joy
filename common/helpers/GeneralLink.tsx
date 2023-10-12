367e08e3f542711ef8aa90ade7e4ba85d8f49704
Ayu Huang <star82620@gmail.com>
2023-10-12 18:39:22 +0800

    update/ adjust general link

--- common/helpers/GeneralLink.tsx
+++ common/helpers/GeneralLink.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { Children, ReactNode } from "react";
 import Link from "next/link";
 
 // 執行此元件 function：給予對應的 props（url、alt、target），輸出結果為 內部連結<Link> 或 外部連結<a>
+// 如果有 alt 就是外部連結，不然就是 Link
 
-interface ILinkProps {
-  url: string;
-  alt: string;
-  content: string;
+interface Props {
+  href: string;
+  children: ReactNode;
   target?: string;
 }
 
-export default function GeneralLink({ url, alt, content, target }: ILinkProps) {
-  return <a>{content}</a>;
+export default function GeneralLink({ href, children, target }: Props) {
+  if (target) {
+    return (
+      <a
+        className=" bg-yellow-400"
+        href={href}
+        target={target}
+        rel="noreferrer noopener"
+      >
+        {children}
+      </a>
+    );
+  } else {
+    return (
+      <Link className=" bg-red-200" href={href}>
+        {children}
+      </Link>
+    );
+  }
 }
-
-//
