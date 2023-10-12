247977ec1575394bc7cc51491325d5f7d2093906
Ayu Huang <star82620@gmail.com>
2023-10-12 17:44:47 +0800

    update/ set font size, font family &  adjust colors to theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,47 +7,91 @@ const config: Config = {
     "./common/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      // primary-yellow
+      "yellow-primary": "#FAF2E2",
+      "yellow-second": "#FEFAF2",
+      "yellow-neutral": "#FFF4C6",
+      "yellow-tone": "#FFE788",
+      "yellow-tint": "#FFD44A",
+      "yellow-dark": "#FFCD00",
+      // secondary-peach
+      "peach-primary": "#F8E9E4",
+      "peach-second": "#FFF4F0",
+      "peach-neutral": "#FFDFD3",
+      "peach-tone": "#FFC8B4",
+      "peach-tint": "#FFA584",
+      "peach-dark": "#FF8356",
+      // Natural Gray
+      "gray-950": "#272725",
+      "gray-900": "#3D3D3C",
+      "gray-800": "#464644",
+      "gray-700": "#504F4E",
+      "gray-600": "#5F5E5B",
+      "gray-500": "#6F6E6B",
+      "gray-400": "#8C8B88",
+      "gray-300": "#B1B0AF",
+      "gray-200": "#D1D1D0",
+      "gray-100": "#E7E7E6",
+      "gray-50": "#F6F6F5",
+      // Complementary color
+      "blue-light": "#3FACFB",
+      "blue-dark": "#1567A3",
+      "green-light": "#26C485",
+      "green-dark": "#3B956B",
+      "purple-light": "#DD84FC",
+      "purple-dark": "#9354A9",
+      danger: "#DD240B",
+    },
+    fontSize: {
+      xl: [
+        "20px",
+        {
+          lineHeight: "1.5",
+          fontWeight: 400,
+        },
+      ],
+      lg: [
+        "18px",
+        {
+          lineHeight: "1.5",
+          fontWeight: 400,
+        },
+      ],
+      md: [
+        "16px",
+        {
+          lineHeight: "1.5",
+          fontWeight: 400,
+        },
+      ],
+      sm: [
+        "14px",
+        {
+          lineHeight: "1.5",
+          fontWeight: 400,
+        },
+      ],
+      xs: [
+        "12px",
+        {
+          lineHeight: "1.5",
+          fontWeight: 400,
+        },
+      ],
+      xxs: [
+        "10px",
+        {
+          lineHeight: "1.5",
+          fontWeight: 400,
+        },
+      ],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        // primary-yellow
-        "yellow-primary": "#FAF2E2",
-        "yellow-second": "#FEFAF2",
-        "yellow-neutral": "#FFF4C6",
-        "yellow-tone": "#FFE788",
-        "yellow-tint": "#FFD44A",
-        "yellow-dark": "#FFCD00",
-        // secondary-peach
-        "peach-primary": "#F8E9E4",
-        "peach-second": "#FFF4F0",
-        "peach-neutral": "#FFDFD3",
-        "peach-tone": "#FFC8B4",
-        "peach-tint": "#FFA584",
-        "peach-dark": "#FF8356",
-        // Natural Gray
-        "gray-950": "#272725",
-        "gray-900": "#3D3D3C",
-        "gray-800": "#464644",
-        "gray-700": "#504F4E",
-        "gray-600": "#5F5E5B",
-        "gray-500": "#6F6E6B",
-        "gray-400": "#8C8B88",
-        "gray-300": "#B1B0AF",
-        "gray-200": "#D1D1D0",
-        "gray-100": "#E7E7E6",
-        "gray-50": "#F6F6F5",
-        // Complementary color
-        "blue-light": "#3FACFB",
-        "blue-dark": "#1567A3",
-        "green-light": "#26C485",
-        "green-dark": "#3B956B",
-        "purple-light": "#DD84FC",
-        "purple-dark": "#9354A9",
-        danger: "#DD240B",
       },
     },
   },
