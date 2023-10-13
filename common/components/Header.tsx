b7f610649b0207af8b00c1c86a72a672c2d9b030
Ayu Huang <star82620@gmail.com>
2023-10-13 11:31:35 +0800

    update/ Header: adjust HTMLtag & remove empty interface

--- common/components/Header.tsx
+++ common/components/Header.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import Logo from "./Logo";
 
-interface Props {}
-
 export default function Header() {
   return (
     <header className="flex justify-center items-center bg-orange-100 py-3 border-b-2 border-stone-950">
       <div className="container flex justify-between">
         <Logo />
-        <div className="flex gap-6">
+        <section className="flex gap-6">
           <a>在 揪遊 上成立店家</a>
           <button>會員按鈕</button>
-        </div>
+        </section>
       </div>
     </header>
   );
