ab94b9ad6cc68bbf38e8a3a402043f66ebed33d9
Ayu Huang <star82620@gmail.com>
2023-10-13 10:05:10 +0800

    update/ adjust html lang -> zh-TW

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="zh-Hant-TW">
+    <Html lang="zh-TW">
       <Head />
       <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&family=Lato:wght@300;400;700&family=Roboto:wght@300;400;500;700&display=swap"
