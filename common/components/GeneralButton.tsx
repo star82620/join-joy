c0c1fe2855a881e63b6b623d167084f8ce9baa21
Ayu Huang <star82620@gmail.com>
2023-10-13 16:59:24 +0800

    feature/ add generalButton component

---
+++ common/components/GeneralButton.tsx
@@ -0,0 +1,28 @@
+import React, { Children, ReactNode } from "react";
+
+// 執行此元件 function：給予對應的 props（type、text、callback function），輸出結果為 button
+
+interface Props {
+  type: "button" | "submit" | "reset";
+  children: ReactNode;
+  onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+}
+
+export default function GeneralButton({
+  type,
+  children,
+  onClick,
+  disabled,
+}: Props) {
+  return (
+    <button
+      className="border text-green-dark"
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
