44aeda109b91bc886a7a61fc07935e7fdfcb12d1
Ayu Huang <star82620@gmail.com>
2023-10-16 20:16:34 +0800

    update/ adjust password visibility switch

--- common/components/FormInput.tsx
+++ common/components/FormInput.tsx
@@ -20,6 +20,9 @@ interface FormInputProps {
 }
 
 export default function FormInput({ formSet }: FormInputProps) {
+  const [visibility, setVisibility] = useState(false);
+  const [passwordType, setPasswordType] = useState("password");
+
   return formSet.map((input) => (
     <label key={input.inputName}>
       <h3 className="text-[18px] md:text-[16px] md:leading-heading ">
@@ -28,22 +31,30 @@ export default function FormInput({ formSet }: FormInputProps) {
       <div className="flex items-center">
         <input
           className="w-full border-b-2 bg-yellow-tint mt-2 py-2 px-3 placeholder:text-gray-400 md:placeholder:text-sm"
-          type={input.type}
+          type={input.type === "password" ? passwordType : input.type}
           name={input.inputName}
           placeholder={input.placeholder}
           required={input.required}
         ></input>
-        <span
+        <input
           className={clsx(
             input.type === "password" ? "inline-block" : "hidden",
+            "appearance-none",
             "w-11 h-7 -ml-11 mt-2 z-10",
-            "bg-eye-off",
-            "bg-center bg-no-repeat bg-yellow-tint"
+            "bg-center bg-no-repeat bg-yellow-tint",
+            !visibility ? "bg-eye-off" : "bg-eye-on"
           )}
-          onClick={() => {
-            console.log("按了要改變 state");
+          type="checkbox"
+          checked={visibility}
+          onChange={(e) => {
+            setVisibility(!visibility);
+            if (!visibility) {
+              setPasswordType("text");
+            } else {
+              setPasswordType("password");
+            }
           }}
-        ></span>
+        ></input>
       </div>
 
       <p className="text-danger mt-2 md:mt-1 text-sm md:text-xs">
