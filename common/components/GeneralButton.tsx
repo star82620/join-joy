cb453757b067942688485e43c019f1a58f97a1fc
Ayu Huang <star82620@gmail.com>
2023-10-16 15:21:57 +0800

    update/ adjust PR comment

--- common/components/GeneralButton.tsx
+++ common/components/GeneralButton.tsx
@@ -3,7 +3,6 @@ import clsx from "clsx";
 
 // 執行此元件 function：給予對應的 props（type、text、callback function），輸出結果為 button
 // 需要判斷的東西：樣式顏色（目前有三種：白黃橘）、按鈕 type、callbackFunction、按鈕內容
-// 有兩種 padding： Header 用（lg）px-4 py-3、一般用 px-2 py-3
 
 interface Props {
   type: "button" | "submit" | "reset";
@@ -11,27 +10,26 @@ interface Props {
   onClick: (event: React.MouseEvent<HTMLElement>) => void;
   disabled?: boolean;
   appearance: "orange" | "yellow" | "light";
-  size?: "lg" | "";
 }
 
 const styleSet = {
   orange: {
     default: "bg-orange-neutral",
-    hover: "bg-orange-tone",
-    active: "bg-orange-tone",
-    disabled: " bg-gray-300",
+    hover: "hover:bg-orange-tone",
+    active: "active:bg-orange-tone",
+    disabled: "disabled:bg-gray-300",
   },
   yellow: {
     default: "bg-yellow-neutral",
-    hover: "bg-yellow-neutral",
-    active: "bg-yellow-neutral",
-    disabled: " bg-gray-300",
+    hover: "hover:bg-yellow-neutral",
+    active: "active:bg-yellow-neutral",
+    disabled: "disabled:bg-gray-300",
   },
   light: {
     default: "bg-yellow-tint",
-    hover: "bg-orange-tint",
-    active: "bg-yellow-primary",
-    disabled: "bg-gray-300",
+    hover: "hover:bg-orange-tint",
+    active: "active:bg-yellow-primary",
+    disabled: "disabled:bg-gray-300",
   },
 };
 
@@ -41,22 +39,21 @@ export default function GeneralButton({
   onClick,
   disabled,
   appearance,
-  size,
 }: Props) {
-  const styleDefault = styleSet[appearance].default;
-  const styleHover = `hover:${styleSet[appearance].hover}`;
-  const styleActive = `active:${styleSet[appearance].active}`;
-  const styleDisabled = `disabled:${styleSet[appearance].disabled}`;
+  const defaultStyle = styleSet[appearance].default;
+  const hoverStyle = styleSet[appearance].hover;
+  const activeStyle = styleSet[appearance].active;
+  const disabledStyle = styleSet[appearance].disabled;
 
   return (
     <button
       className={clsx(
         "border-2 border-gray-950 shadow-btn",
-        size === "lg" ? "px-4 py-3" : "px-2 py-3",
-        styleDefault,
-        styleHover,
-        styleActive,
-        disabled ? styleDisabled : "",
+        "px-2 py-3",
+        defaultStyle,
+        hoverStyle,
+        activeStyle,
+        disabled ? disabledStyle : "",
         "active:shadow-none active:-mr-0.5 active:ml-0.5 active:mt-0.5 active:-mb-0.5"
       )}
       type={type}
