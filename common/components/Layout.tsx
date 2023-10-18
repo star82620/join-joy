c5d68e21291cf74b858128f24a9c1345ea0fe249
Ayu Huang <star82620@gmail.com>
2023-10-18 16:08:16 +0800

    update/ Layout: add main bg color

--- common/components/Layout.tsx
+++ common/components/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }: Props) {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="grow">{children}</main>
+      <main className="grow bg-yellow-tint">{children}</main>
       <Footer />
     </div>
   );
