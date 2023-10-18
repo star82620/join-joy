d430ede161937436d42bf4466ee57cec1eb44daa
Ayu Huang <star82620@gmail.com>
2023-10-18 11:53:11 +0800

    update/ adjust component Header style

--- common/components/Header.tsx
+++ common/components/Header.tsx
@@ -1,18 +1,37 @@
 import React from "react";
 import Logo from "./WebsiteLogo";
+import Image from "next/image";
 import Button from "./GeneralButton";
+import Link from "./GeneralLink";
 
 const test = () => console.log("I see");
 
 export default function Header() {
   return (
-    <header className="flex justify-center items-center bg-orange-100 py-3 border-b-2 border-stone-950">
-      <div className="container flex justify-between">
-        <Logo width="80" height="80" />
-        <section className="flex gap-6">
-          <a>在 揪遊 上成立店家</a>
+    <header className="flex justify-center items-center bg-yellow-dark border-b-2 border-stone-950">
+      <div className="flex justify-between items-center container px-12 py-5 md:px-2 md:py-3">
+        <Link href="/">
+          <Logo width="38" height="38" />
+        </Link>
+        <section className="flex items-center gap-6">
+          <Link href="/create-group" className="md:hidden">
+            在 揪遊 上成立店家
+          </Link>
           <Button type="button" onClick={test} appearance="light">
-            會員按鈕
+            <Image
+              src="/images/icon-header-user.svg"
+              alt="user"
+              width="36"
+              height="24"
+              className="inline md:hidden border-r-2 border-gray-950 pr-3 mr-3"
+            ></Image>
+            <Image
+              src="/images/icon-header-menu.svg"
+              alt="menu"
+              width="24"
+              height="24"
+              className="inline"
+            ></Image>
           </Button>
         </section>
       </div>
