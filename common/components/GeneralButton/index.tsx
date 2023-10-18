344df0e62e85858215b9c4fe49f5d680a48396ca
Ayu Huang <star82620@gmail.com>
2023-10-18 15:39:26 +0800

    update/ split component to index & data

--- common/components/GeneralButton/index.tsx
+++ common/components/GeneralButton/index.tsx
@@ -1,38 +1,9 @@
 import React, { Children, ReactNode } from "react";
 import clsx from "clsx";
+import { PropsType, dataSet } from "./data";
 
 // 執行此元件 function：給予對應的 props（type、text、callback function），輸出結果為 button
 // 需要判斷的東西：樣式顏色（目前有三種：白黃橘）、按鈕 type、callbackFunction、按鈕內容
-
-type PropsType = {
-  type: "button" | "submit" | "reset";
-  children: ReactNode;
-  onClick: (event: React.MouseEvent<HTMLElement>) => void;
-  isDisabled?: boolean;
-  appearance: "orange" | "yellow" | "light";
-  className?: string;
-};
-
-const styleSet = {
-  orange: {
-    default: "bg-orange-neutral",
-    hover: "hover:bg-orange-tone",
-    active: "active:bg-orange-tone",
-    disabled: "disabled:bg-gray-300",
-  },
-  yellow: {
-    default: "bg-yellow-neutral",
-    hover: "hover:bg-yellow-neutral",
-    active: "active:bg-yellow-neutral",
-    disabled: "disabled:bg-gray-300",
-  },
-  light: {
-    default: "bg-yellow-tint",
-    hover: "hover:bg-orange-tint",
-    active: "active:bg-yellow-primary",
-    disabled: "disabled:bg-gray-300",
-  },
-};
 
 export default function GeneralButton({
   type,
@@ -51,7 +22,7 @@ export default function GeneralButton({
     <button
       className={clsx(
         "border-2 border-gray-950 shadow-btn",
-        "px-3 py-2",
+        "px-3 py-2 h-fit",
         defaultStyle,
         hoverStyle,
         activeStyle,
