ada91718f220af90ba24687441bd94e08176ded5
Ayu Huang <star82620@gmail.com>
2023-10-19 09:01:06 +0800

    fix/ recover GeneralLink

---
+++ common/components/GeneralLink.tsx
@@ -0,0 +1,43 @@
+import React, { Children, ReactNode } from "react";
+import Link from "next/link";
+import clsx from "clsx";
+
+// 執行此元件 function：給予對應的 props（url、target），輸出結果為 內部連結<Link> 或 外部連結<a>
+
+interface Props {
+  href: string;
+  children: ReactNode;
+  target?: string;
+  className?: string;
+}
+
+export default function GeneralLink({
+  href,
+  children,
+  target,
+  className,
+}: Props) {
+  // 如果有 href 但沒有 target => 內部連結
+  if (href && !target)
+    return (
+      <Link className={clsx("underline", className)} href={href}>
+        {children}
+      </Link>
+    );
+
+  // 如果有 href 和 target => 外部連結
+  if (href && target)
+    return (
+      <a
+        className={clsx("underline", className)}
+        href={href}
+        target={target}
+        rel="noreferrer noopener"
+      >
+        {children}
+      </a>
+    );
+
+  // 如果不符合上述條件，什麼都不給 return null
+  return null;
+}
