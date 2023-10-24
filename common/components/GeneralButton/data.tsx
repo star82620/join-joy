fdd474a536bd8827b6a87895a2cc73151e79d0a2
Ayu Huang <star82620@gmail.com>
2023-10-24 23:29:16 +0800

    update/ GeneralButton setting

--- common/components/GeneralButton/data.tsx
+++ common/components/GeneralButton/data.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export type ButtonType = {
   type: "button" | "submit" | "reset";
-  appearance: "orange" | "yellow" | "light";
+  appearance: "orange" | "yellow" | "light" | "black";
   children: ReactNode;
   onClick?: (event: React.MouseEvent<HTMLElement>) => void | undefined;
   isDisabled?: boolean;
@@ -16,17 +16,27 @@ export const dataSet = {
     hover: "hover:bg-orange-tone",
     active: "active:bg-orange-tone",
     disabled: "disabled:bg-gray-300",
+    textColor: "text-gray-950",
   },
   yellow: {
     default: "bg-yellow-neutral",
     hover: "hover:bg-yellow-neutral",
     active: "active:bg-yellow-neutral",
     disabled: "disabled:bg-gray-300",
+    textColor: "text-gray-950",
   },
   light: {
     default: "bg-yellow-tint",
     hover: "hover:bg-orange-tint",
     active: "active:bg-yellow-primary",
     disabled: "disabled:bg-gray-300",
+    textColor: "text-gray-950",
+  },
+  black: {
+    default: "bg-gray-950",
+    hover: "hover:bg-gray-950",
+    active: "active:bg-gray-950",
+    disabled: "disabled:bg-gray-300",
+    textColor: "text-white",
   },
 };
