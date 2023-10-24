e5f6b9a01ce3c02aec1acb03edf3ea659320de64
Ayu Huang <star82620@gmail.com>
2023-10-24 11:33:46 +0800

    feature/ create modules

--- pages/create-group/index.jsx
+++ pages/create-group/index.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import Layout from "@/common/components/Layout"
+import CreateGroup from "@/modules/CreateGroup"
 
-export default function CreateGroup() {
-  return <div>CreateGroup</div>;
+export default function CreateGroupPage() {
+  return (
+    <Layout
+      pageCategory="create-group"
+      mainClassName="py-[88px] lg:py-18 md:py-16 sm:py-14"
+    >
+      <CreateGroup />
+    </Layout>
+  )
 }
