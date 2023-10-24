cb2906d7ce1a9a30b6beb1c9c94f5994dd0f490a
Ayu Huang <star82620@gmail.com>
2023-10-25 02:17:00 +0800

    update/ adjust layout

--- common/components/Wrapper.tsx
+++ common/components/Wrapper.tsx
@@ -2,7 +2,7 @@ import React, { Children, ReactNode } from "react";
 
 type WrapperProps = {
   title: string;
-  titleStyle: string;
+  titleStyle?: string;
   children: ReactNode;
 };
 
@@ -10,10 +10,12 @@ type WrapperProps = {
 
 export default function Wrapper({ title, titleStyle, children }: WrapperProps) {
   return (
-    <div className="m-auto w-fit rounded-t-lg shadow-window">
+    <div className="w-fit rounded-t-lg shadow-window">
       <section className="flex justify-between items-center bg-yellow-neutral border-[3px] border-b-2 rounded-t-lg px-3 py-2 text-center">
         <div className="w-6 h-6 rounded-full bg-white"></div>
-        <h2 className={titleStyle}>{title}</h2>
+        <h2 className={`font-semibold text-xl leading-[1.2] ${titleStyle}`}>
+          {title}
+        </h2>
         <div className="w-6 h-6 rounded-full bg-white"></div>
       </section>
       <section className="bg-yellow-dark border-[3px] border-t-0 px-[100px] py-16 lg:px-14 lg:py-12 md:px-8 md:py-8 sm:px-4 sm:py-6">
---
+++ modules/UserProfile/index.tsx
@@ -0,0 +1,153 @@
+import React from "react";
+import Image from "next/image";
+import Wrapper from "@/common/components/Wrapper";
+import Button from "@/common/components/GeneralButton";
+import Link from "@/common/components/GeneralLink";
+
+export default function UserProfile() {
+  return (
+    <div className="flex md:flex-col justify-center items-start gap-9 container">
+      <Wrapper title="關於我">
+        <div className="flex flex-col items-center gap-6">
+          <div className="w-28 h-28 rounded-full border-2 border-white outline outline-2 outline-gray-950 relative">
+            <Image
+              src="/images/photo-user-000.png"
+              alt="name"
+              fill={true}
+              className="object-contain rounded-full"
+            />
+          </div>
+          <p>name</p>
+          <p>country</p>
+          <Button type="button" appearance="orange">
+            追蹤
+          </Button>
+          <div>perfer games</div>
+          <div>簡介</div>
+        </div>
+      </Wrapper>
+      <section className="flex flex-col w-full relative min-h-full">
+        <div className="flex items-start absolute ">
+          {/* 只有第一個才有露出左邊圓角 */}
+          <span
+            className="pt-2 pb-[9.5px] px-6 rounded-t-md border-[3px] border-b-0 bg-yellow-dark text-lg font-semibold leading-1 z-10"
+            data-active="groups"
+          >
+            <Image
+              src="/images/icon-lists.svg"
+              width="24"
+              height="24"
+              alt="list"
+              className="inline align-middle"
+            />
+            <span className="ml-2 align-middle">揪團清單</span>
+          </span>
+          <span
+            className="pt-2 pb-2 px-6 rounded-tr-md border-[3px] border-b-0 border-gray-400 border-l-0 -ml-3  text-lg font-semibold text-gray-600 bg-gray-200 leading-1"
+            data-active="comments"
+          >
+            <Image
+              src="/images/icon-comments.svg"
+              width="24"
+              height="24"
+              alt="comments"
+              className="inline align-middle"
+            />
+            <span className="ml-2 align-middle">綜合評價</span>
+          </span>
+        </div>
+        <section className="p-6 bg-yellow-dark border-[3px] border-t-2 w-full mt-12">
+          <div>
+            <h3 className="text-lg font-semibold">正在開團中</h3>
+            <div className="flex flex-col mt-4 gap-3">
+              <section className="flex items-center p-4 border-2 shadow-btn rounded-[4px] bg-yellow-tint">
+                <div className="text-lg font-bold">2023/10/2</div>
+                <div className="border-l-2 border-gray-500 pl-4 ml-4 grow">
+                  <p className="text-lg font-bold">輕鬆派對揪友團</p>
+                  <p>
+                    <Image
+                      src="/images/icon-location-light.svg"
+                      alt="location-light"
+                      width="18"
+                      height="18"
+                      className="inline align-middle"
+                    />
+                    <span className="align-middle text-xs text-gray-400 ml-2">
+                      六角學院桌遊店
+                    </span>
+                  </p>
+                </div>
+                <div className="font-semibold">目前人數： 6 /12</div>
+              </section>
+              <section className="flex items-center p-4 border-2 shadow-btn rounded-[4px] bg-yellow-tint">
+                <div className="text-lg font-bold">2023/10/2</div>
+                <div className="border-l-2 border-gray-500 pl-4 ml-4 grow">
+                  <p className="text-lg font-bold">輕鬆派對揪友團</p>
+                  <p>
+                    <Image
+                      src="/images/icon-location-light.svg"
+                      alt="location-light"
+                      width="18"
+                      height="18"
+                      className="inline align-middle"
+                    />
+                    <span className="align-middle text-xs text-gray-400 ml-2">
+                      六角學院桌遊店
+                    </span>
+                  </p>
+                </div>
+                <div className="font-semibold">目前人數： 6 /12</div>
+              </section>
+            </div>
+          </div>
+          <div className="mt-8">
+            <h3 className="text-lg font-semibold">過往揪團紀錄</h3>
+            <section className=" mt-4">
+              <h4 className="text-xl font-bold ml-2">2023</h4>
+              <section className="flex items-center p-4 ">
+                <div className="text-lg font-bold">10/2</div>
+                <div className="border-l-2 border-gray-500 pl-4 ml-4 grow">
+                  <p className="text-md font-bold font-semibold">
+                    輕鬆派對揪友團
+                  </p>
+                  <p>
+                    <Image
+                      src="/images/icon-location-dark.svg"
+                      alt="location-light"
+                      width="20"
+                      height="20"
+                      className="inline align-middle"
+                    />
+                    <span className="align-middle text-sm text-gray-400 ml-2">
+                      <Link href="/">六角學院桌遊店</Link>
+                    </span>
+                  </p>
+                </div>
+              </section>
+              <section className="flex items-center p-4 ">
+                <div className="text-lg font-bold">10/2</div>
+                <div className="border-l-2 border-gray-500 pl-4 ml-4 grow">
+                  <p className="text-md font-bold font-semibold">
+                    輕鬆派對揪友團
+                  </p>
+                  <p>
+                    <Image
+                      src="/images/icon-location-dark.svg"
+                      alt="location-light"
+                      width="20"
+                      height="20"
+                      className="inline align-middle"
+                    />
+                    <span className="align-middle text-sm text-gray-400 ml-2">
+                      <Link href="/">六角學院桌遊店</Link>
+                    </span>
+                  </p>
+                </div>
+              </section>
+            </section>
+          </div>
+        </section>
+      </section>
+    </div>
+  );
+}
