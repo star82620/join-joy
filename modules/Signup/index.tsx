e73a55ed2ef86e6136614b00dff69cc57a2244ad
Ayu Huang <star82620@gmail.com>
2023-10-25 09:05:49 +0800

    update/ adjust Wrapper props: contentStyle & hideShadow

--- modules/Signup/index.tsx
+++ modules/Signup/index.tsx
@@ -11,6 +11,7 @@ export default function Signup() {
       <Wrapper
         title={title}
         titleStyle="text-[20px] md:text-[18px] md:leading-heading"
+        mainStyle="px-[100px] py-16 lg:px-14 lg:py-12 md:p-8 sm:px-4 sm:py-6"
       >
         <Form inputSet={inputSet} btnSet={btnSet} apiParams={apiParams} />
         <p className="mt-3 text-center text-sm md:text-xs">
