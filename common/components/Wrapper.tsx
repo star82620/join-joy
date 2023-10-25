b3b22a9b50f9b1522c44c5ec4a36f86346d16172
Ayu Huang <star82620@gmail.com>
2023-10-25 12:09:11 +0800

    update/ adjust component Wrapper layout

--- common/components/Wrapper.tsx
+++ common/components/Wrapper.tsx
@@ -2,21 +2,33 @@ import React, { Children, ReactNode } from "react";
 
 type WrapperProps = {
   title: string;
-  titleStyle: string;
+  titleStyle?: string;
   children: ReactNode;
+  contentStyle?: string;
+  hideShadow?: boolean;
 };
 
 // FrameWindow 框框寬度是看內容的
 
-export default function Wrapper({ title, titleStyle, children }: WrapperProps) {
+export default function Wrapper({
+  title,
+  titleStyle,
+  children,
+  contentStyle,
+  hideShadow,
+}: WrapperProps) {
   return (
-    <div className="m-auto w-fit rounded-t-lg shadow-window">
+    <div
+      className={`w-full rounded-t-lg ${!hideShadow ? "shadow-window" : ""}`}
+    >
       <section className="flex justify-between items-center bg-yellow-neutral border-[3px] border-b-2 rounded-t-lg px-3 py-2 text-center">
         <div className="w-6 h-6 rounded-full bg-white"></div>
-        <h2 className={titleStyle}>{title}</h2>
+        <h2 className={`font-semibold text-xl ${titleStyle}`}>{title}</h2>
         <div className="w-6 h-6 rounded-full bg-white"></div>
       </section>
-      <section className="bg-yellow-dark border-[3px] border-t-0 px-[100px] py-16 lg:px-14 lg:py-12 md:px-8 md:py-8 sm:px-4 sm:py-6">
+      <section
+        className={`bg-yellow-dark border-[3px] border-t-0 ${contentStyle}`}
+      >
         {children}
       </section>
     </div>
