ffbc6d50a248eca86f875378ba41890574fea183
Ayu Huang <star82620@gmail.com>
2023-10-25 12:02:42 +0800

    update/ adjust signup layout

--- modules/Signup/index.tsx
+++ modules/Signup/index.tsx
@@ -11,15 +11,16 @@ export default function Signup() {
       <Wrapper
         title={title}
         titleStyle="text-[20px] md:text-[18px] md:leading-heading"
-        mainStyle="px-[100px] py-16 lg:px-14 lg:py-12 md:p-8 sm:px-4 sm:py-6"
       >
-        <Form inputSet={inputSet} btnSet={btnSet} apiParams={apiParams} />
-        <p className="mt-3 text-center text-sm md:text-xs">
-          註冊即表示您閱讀並同意 <b>服務條款</b> 及 <b>隱私權政策</b>
-        </p>
-        <p className="mt-8 text-center text-md font-semibold">
-          已有帳號？ <Link href="/login">點擊登入</Link>
-        </p>
+        <div className="px-[100px] py-16 lg:px-14 lg:py-12 md:p-8 sm:px-4 sm:py-6">
+          <Form inputSet={inputSet} btnSet={btnSet} apiParams={apiParams} />
+          <p className="mt-3 text-center text-sm md:text-xs">
+            註冊即表示您閱讀並同意 <b>服務條款</b> 及 <b>隱私權政策</b>
+          </p>
+          <p className="mt-8 text-center text-md font-semibold">
+            已有帳號？ <Link href="/login">點擊登入</Link>
+          </p>
+        </div>
       </Wrapper>
     </div>
   );
