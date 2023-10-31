8b149f471e2a45651153a6131253529ba0da7aad
Ayu Huang <star82620@gmail.com>
2023-10-31 14:34:20 +0800

    feature/ create page

---
+++ pages/group/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import GroupProfile from "@/modules/GroupProfile";
+
+export default function GroupProfilePage() {
+  return (
+    <div>
+      <GroupProfile />
+    </div>
+  );
+}
