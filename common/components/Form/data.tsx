5480301523ef1638f33f0fd799a6465f7bea10f8
Ayu Huang <star82620@gmail.com>
2023-11-02 15:48:28 +0800

    fix/ adjust deployment error

--- common/components/Form/data.tsx
+++ common/components/Form/data.tsx
@@ -7,7 +7,7 @@ export type InputType = {
   inputName: string;
   placeholder: string;
   required: boolean;
-  errorMsg?: string | Record<string, string>;
+  errorMsg?: string;
   pattern?: RegExp;
 };
 
