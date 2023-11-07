de35b6fe850896e29dad73ec300727e80f04bb87
Ayu Huang <star82620@gmail.com>
2023-11-07 22:16:01 +0800

    update/ set input style

--- modules/UserCenter/ProfileSetting.tsx
+++ modules/UserCenter/ProfileSetting.tsx
@@ -6,7 +6,7 @@ import Button from "@/common/components/GeneralButton";
 import Link from "@/common/components/GeneralLink";
 
 export default function ProfileSetting() {
-  const data = fetchApi(userDataKey);
+  // const data = fetchApi(userDataKey);
 
   const inputTitleStyle = "text-lg md:text-md mb-2 md:mb-1";
   const inputDescStyle = "text-sm md:text-xs text-gray-500";
@@ -15,25 +15,31 @@ export default function ProfileSetting() {
     <section className="p-8 md:px-4">
       <form className="flex flex-col gap-10">
         <div className="flex md:flex-col justify-between gap-6">
-          <div className="flex flex-col gap-10 md:gap-6 md:order-2">
+          <div className="w-full flex flex-col gap-10 md:gap-6 md:order-2">
             <div>
-              <h3 className={`${inputTitleStyle} mb-2 md:mb-1`}>你的名字</h3>
-              <input type="text" />
+              <h3 className={`${inputTitleStyle} `}>你的名字</h3>
+              <input type="text" className="inputStyle mt-2 md:mt-1" />
             </div>
             <div>
-              <h3 className={`${inputTitleStyle} mb-2 md:mb-1`}>帳號</h3>
-              <input type="email" readOnly />
+              <h3 className={`${inputTitleStyle}`}>帳號</h3>
+              <input
+                type="email"
+                className="inputStyle mt-2 md:mt-1"
+                readOnly
+              />
             </div>
             <div>
-              <h3 className={`${inputTitleStyle} mb-4`}>密碼</h3>
+              <h3 className={`${inputTitleStyle}`}>密碼</h3>
               <Link href="/" className="no-underline">
-                <span className="text-blue-dark md:text-sm">重設新的密碼</span>
+                <span className="text-blue-dark md:text-sm mt-4">
+                  重設新的密碼
+                </span>
               </Link>
             </div>
             <div>
-              <h3 className={`${inputTitleStyle} mb-2 md:mb-1`}>地區</h3>
-              <p className={`${inputDescStyle} mb-2`}>最多選擇 3 個</p>
-              <select>
+              <h3 className={`${inputTitleStyle}`}>地區</h3>
+              <p className={`${inputDescStyle} mt-2 md:mt-1`}>最多選擇 3 個</p>
+              <select className="mt-2">
                 <option>city</option>
               </select>
             </div>
@@ -67,17 +73,25 @@ export default function ProfileSetting() {
             <div>一顆一顆</div>
           </div>
           <div>
-            <h3 className={`${inputTitleStyle} mb-2 md:mb-1`}>個人簡介</h3>
-            <p className={`flex justify-between mb-2`}>
-              <span className={inputDescStyle}>讓更多人認識你唷</span>
+            <h3 className={`${inputTitleStyle}`}>個人簡介</h3>
+            <p className={`flex justify-between mt-2 md:mt-2`}>
+              <span className={`${inputDescStyle}`}>讓更多人認識你唷</span>
               <span className="text-sm md:text-xs font-bold">0/100</span>
             </p>
-            <textarea placeholder="輸入你的自我介紹"></textarea>
+            <textarea
+              className="inputStyle mt-2"
+              placeholder="輸入你的自我介紹"
+            ></textarea>
           </div>
         </div>
       </form>
       <div className="flex gap-6 md:gap-4 justify-center items-center mt-10 md:mt-8">
-        <Button type="submit" appearance="white" className="w-full" rounded>
+        <Button
+          type="submit"
+          appearance="white"
+          className="w-full text-lg"
+          rounded
+        >
           放棄變更
         </Button>
         <Button type="submit" appearance="black" className="w-full" rounded>
