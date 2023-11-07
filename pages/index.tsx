91520a6365a00f9e93af069b2517fc32a1f839d1
Ayu Huang <star82620@gmail.com>
2023-11-07 18:33:48 +0800

    fix/ deployment error: @type/ js-cookie

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import Image from "next/image";
 import { AuthContext } from "@/common/contexts/AuthProvider";
 import Layout from "@/common/components/Layout";
 
@@ -33,10 +34,7 @@ export default function Home() {
           </li>
         </ul>
 
-        <div className=" max-w-[826px]">
-          hi!{userData?.nickName}
-          {/* <Image src={a} alt={userData.nickName} width="100" height="100" /> */}
-        </div>
+        <div className=" max-w-[826px]">hi!{userData?.nickName}</div>
       </Layout>
     </>
   );
