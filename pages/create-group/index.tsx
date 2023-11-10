664a958c68e6bdcd24d6f1bc50bf82ae95756dbc
Ayu Huang <star82620@gmail.com>
2023-11-10 15:37:17 +0800

    update/ getServerSideProps

---
+++ pages/create-group/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Layout from "@/common/components/Layout";
+import CreateGroup from "@/modules/CreateGroup";
+import fetchApi, { apiParamsType } from "@/common/helpers/fetchApi";
+
+export async function getServerSideProps(context) {
+  const { req } = context;
+  const { authToken } = req.cookies;
+
+  const apiParams: apiParamsType = {
+    apiPath: "/auth/checkLoginStatus",
+    method: "GET",
+  };
+  const res = await fetchApi(apiParams);
+  const data = res ? res : null;
+
+  return {
+    props: {
+      res: data,
+      authToken: authToken,
+    },
+  };
+}
+
+export default function CreateGroupPage(props) {
+  return (
+    <Layout
+      pageCategory="create-group"
+      mainClassName="py-[88px] lg:py-18 md:py-16 sm:py-14"
+    >
+      <CreateGroup />
+    </Layout>
+  );
+}
