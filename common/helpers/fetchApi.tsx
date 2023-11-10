d8682449883b7d234d0435ac33f57592053bcf1c
Ayu Huang <star82620@gmail.com>
2023-11-10 09:08:44 +0800

    update/ fetchApi body type

--- common/helpers/fetchApi.tsx
+++ common/helpers/fetchApi.tsx
@@ -1,40 +1,45 @@
 import getToken from "@/common/helpers/getToken";
 
-const token: string | null = getToken();
-
-// 要用的話要寫這一包資料
-// const apiParams = {
-//   apiPath: "/users/sign_up",
-//   method: "POST",
-//   data: {...},
-// };
-// const data = fetchApi(apiParams);
-
 export type apiParamsType = {
   apiPath: string;
   method: "GET" | "POST" | "PATCH" | "DELETE";
-  data?: Record<string, string> | string;
+  data?: unknown;
 };
+
+const token: string | null = getToken();
 
 export default async function fetchApi(apiParams: apiParamsType) {
   const { apiPath, method, data } = apiParams;
+
   const url = `${process.env.NEXT_PUBLIC_API_URL}${apiPath}`;
+
   const headers: Record<string, string> = {
     "Content-Type": "application/json",
   };
+
   if (token) {
-    headers["Authorization"] = token ? token : "";
+    headers["Authorization"] = token;
   }
+
+  let body;
+
+  if (typeof data === "object" && data !== null) {
+    body = JSON.stringify(data);
+  } else if (typeof data === "string") {
+    body = data;
+  }
+
   const requestOptions = {
     method: method,
     headers: headers,
-    body: JSON.stringify(data),
+    body: body,
   };
 
   try {
     const res = await fetch(url, requestOptions);
     const result = await res.json();
-    console.log("result", result);
+
+    console.log("fetchApi", result);
 
     return result;
   } catch (error) {
