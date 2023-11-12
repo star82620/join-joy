29a6b62134ead6733d615d200d17d55438159550
Ayu Huang <star82620@gmail.com>
2023-11-12 18:48:35 +0800

    update/ add groupTags of wordsIndex

---
+++ constants/wordsIndex.tsx
@@ -0,0 +1,9 @@
+export const gameTags = {
+  beginnerTag: "新手團",
+  expertTag: "老手團",
+  practiceTag: "經驗切磋",
+  openTag: "不限定",
+  tutorialTag: "教學團",
+  casualTag: "輕鬆",
+  competitiveTag: "競技",
+};
