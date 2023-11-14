b5ded0e44e0bfabf718a4cd83f084f1b7c504228
Ayu Huang <star82620@gmail.com>
2023-11-14 23:26:54 +0800

    update/ adjust MemberCard

---
+++ modules/UserCenter/MyGroups/ManageGroup/MemberCard.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Button from "@/common/components/GeneralButton";
+import ProfileImg from "@/common/components/ProfileImg";
+
+export default function MemberCard({ groupStatus, userData }) {
+  // 按鈕狀態改變
+  // 如果是 "pending"：拒絕、接受
+  // 如果是 "member"：缺席、出席
+  const btns = {
+    pending: [
+      {
+        text: "拒絕",
+        appearance: "white",
+        handler: "",
+      },
+      {
+        text: "接受",
+        appearance: "black",
+        handler: "",
+      },
+    ],
+    member: [
+      { text: "缺席", appearance: "white", handler: "" },
+      { text: "出席", appearance: "black", handler: "" },
+    ],
+  };
+
+  const displayBtn = btns["member"];
+  const rejectBtnAppearance = displayBtn[0].appearance;
+  const rejectBtnText = displayBtn[0].text;
+  const rejectBtnHandler = displayBtn[0].handler;
+  const acceptBtnAppearance = displayBtn[1].appearance;
+  const acceptBtnText = displayBtn[1].text;
+  const acceptBtnHandler = displayBtn[1].handler;
+  const isBtnDisabled = false;
+
+  return (
+    <li className="w-full flex flex-col md:flex-row md:justify-between gap-2 p-2 rounded border-2 bg-yellow-tint shadow-btn">
+      <div className="flex items-center gap-4 md:gap-2">
+        <ProfileImg
+          src=""
+          alt=""
+          widthProp="w-10 md:w-9"
+          heightProp="h-10 md:h-9"
+        />
+        <div className="font-semibold">
+          <p className="text-gray-600 md:text-xs">團主</p>
+          <p className="md:text-sm">鳩鳩</p>
+        </div>
+      </div>
+      <div className="flex gap-2 md:w-[128px]">
+        <Button
+          type="button"
+          appearance={rejectBtnAppearance}
+          onClick={rejectBtnHandler}
+          rounded
+          isDisabled={isBtnDisabled}
+          className="w-full"
+        >
+          {rejectBtnText}
+        </Button>
+        <Button
+          type="button"
+          appearance={acceptBtnAppearance}
+          onClick={acceptBtnHandler}
+          rounded
+          isDisabled={isBtnDisabled}
+          className="w-full"
+        >
+          {acceptBtnText}
+        </Button>
+      </div>
+    </li>
+  );
+}
--- modules/UserCenter/MyGroups/ManageGroup/index.tsx
+++ modules/UserCenter/MyGroups/ManageGroup/index.tsx
@@ -1,20 +1,41 @@
 import React from "react";
-import ModalWrapper from "@/common/components/ModalWrapper";
-import titles from "@/constants/groupProfileTitles";
 import Image from "@/common/components/FillImage";
 import Button from "@/common/components/GeneralButton";
+import ModalWrapper from "@/common/components/ModalWrapper";
+import titles from "@/constants/groupProfileTitles";
+import GameList from "@/common/components/GameList";
+import MemberCard from "./MemberCard";
+
+// 審核團員列表會在團的狀態為已成團、已預約後消失
+// 參加者列表的按鈕會在活動時間開始前一小時開放
+// 活動時間前一小時的時間，有沒有
+// 活動是 11-14 11:30 ，前一個小時是 11-14 10:30
+// 現在時間 11-14 11:00
+const isWithinOneHour = (time: Date) => {
+  // 現在時間
+  const now = new Date();
+  // 現在時間減去一小時
+  const oneHourAgo = new Date(now.getTime() - 60 * 60 * 1000);
+
+  // 確認指定時間有沒有在一小時內
+  const result = time >= oneHourAgo && time <= now;
+
+  return result;
+};
 
 // 送出預約
 const handleSubmitReserve = () => {};
+
+const status = "pending";
 
 export default function ManageGroup() {
   const reserveInfoTitles = ["location", "date", "time", "cost"];
 
   return (
-    <section className="container flex gap-9 md:gap-4 pt-8 pb-15">
+    <section className="container flex md:flex-col gap-9 md:gap-4 pt-8 pb-15">
       <div className="flex flex-col gap-9 md:gap-4 grow">
         <ModalWrapper title="店家預約資訊" layout="primary">
-          <div className="flex flex-col gap-8 md:gap-6 px-10 py-6 md:p-4">
+          <div className="flex flex-col justify-start gap-8 md:gap-6 px-10 py-6 md:p-4">
             <h3 className="text-sm">目前揪團狀態：開團中tagggg</h3>
             <ul className="flex md:flex-col gap-12 md:gap-6">
               {reserveInfoTitles.map((title) => {
@@ -38,25 +59,46 @@ export default function ManageGroup() {
               })}
             </ul>
 
-            <Button
-              type="button"
-              appearance="orange"
-              onClick={handleSubmitReserve}
-            >
-              <span className="w-[182px] md:w-full text-xl">送出預約</span>
-            </Button>
+            <div className="flex flex-col items-center">
+              <Button
+                type="button"
+                appearance="orange"
+                onClick={handleSubmitReserve}
+                className="w-1/2 md:w-full text-xl"
+              >
+                送出預約
+              </Button>
+              <p className="mt-3 text-gray-600 font-semibold">
+                預約後不可再接受團員進出
+              </p>
+            </div>
           </div>
         </ModalWrapper>
         <ModalWrapper title="編輯揪團詳細資訊" layout="primary">
-          <div className="px-14 py-10 md:p-4">fff</div>
+          <div className="px-14 py-10 md:p-4">
+            就很開團表單
+            <GameList category="form"></GameList>
+          </div>
         </ModalWrapper>
       </div>
-      <div className="w-[304px] flex flex-col gap-9 md:gap-4">
+      <div className="w-[304px] md:w-full flex flex-col gap-9 md:gap-4">
         <ModalWrapper title="審核團員列表" layout="secondary">
-          <div className="p-4">fff</div>
+          <div className="flex flex-col items-center gap-2 p-4">
+            <p className="font-semibold">申請人數總共 0 人</p>
+            <p className="text-sm text-blue-dark font-semibold ">全部確認</p>
+
+            <ul className="w-full">ccc{/* <MemberCard /> */}</ul>
+          </div>
         </ModalWrapper>
         <ModalWrapper title="參加者列表" layout="secondary">
-          <div className="p-4">fff</div>
+          <div className="flex flex-col items-center gap-2 p-4">
+            <p className="font-semibold ">目前人數： 0 人</p>
+            <p className="text-sm font-semibold text-gray-800">
+              活動當天前1小時開放點名
+            </p>
+
+            <ul className="w-full">{/* <MemberCard /> */}</ul>
+          </div>
         </ModalWrapper>
       </div>
     </section>
