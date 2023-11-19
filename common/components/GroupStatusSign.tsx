7702a4548690d26ff2622ffb5398118e3a12f7b2
Ayu Huang <star82620@gmail.com>
2023-11-20 01:27:59 +0800

    fix/ deployment error: type

--- common/components/GroupStatusSign.tsx
+++ common/components/GroupStatusSign.tsx
@@ -8,23 +8,41 @@ import {
 
 export type GroupStatusSignProps = {
   category: "group" | "mine";
-  status: string;
+  status: GroupStatusKeyType | MyGroupStatusKeyType;
 };
 
-export default function GroupStatusSign({
-  category,
-  status,
-}: GroupStatusSignProps) {
-  const reference = category === "mine" ? myGroupStatusSet : groupStatusSet;
+function isMyGroupStatusKey(key: any): key is MyGroupStatusKeyType {
+  return ["pending", "member", "closed"].includes(key);
+}
 
-  const color = reference[status]?.style;
+function isGroupStatusKey(key: any): key is GroupStatusKeyType {
+  return ["已結束", "已失效", "已預約", "開團中"].includes(key);
+}
 
-  const text = reference[status]?.text;
-
+function renderStatusSign(color: string, text: string) {
   return (
     <div className="flex justify-center items-center whitespace-nowrap flex-shrink-0">
       <div className={`groupStatusDot ${color}`}></div>
       <div className="text-xs">{text}</div>
     </div>
   );
+}
+
+export default function GroupStatusSign({
+  category,
+  status,
+}: GroupStatusSignProps) {
+  if (category === "mine" && isMyGroupStatusKey(status)) {
+    const color = myGroupStatusSet[status].style;
+    const text = myGroupStatusSet[status].text;
+
+    return renderStatusSign(color, text);
+  } else if (category === "group" && isGroupStatusKey(status)) {
+    const color = groupStatusSet[status].style;
+    const text = groupStatusSet[status].text;
+
+    return renderStatusSign(color, text);
+  } else {
+    return null;
+  }
 }
