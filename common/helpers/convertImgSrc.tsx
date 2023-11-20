4c54c09d7d76020dd2a09cfdaf6c3d5fdeff7511
Ayu Huang <star82620@gmail.com>
2023-11-20 17:31:21 +0800

    update/ adjust convertImgSrx

--- common/helpers/convertImgSrc.tsx
+++ common/helpers/convertImgSrc.tsx
@@ -1,11 +1,17 @@
 import { defaultImages } from "@/constants/defaultImages";
 
 export default function convertImgSrc(img: string | null) {
-  const convertImgSrc = `${process.env.NEXT_PUBLIC_API_URL}${img}`;
+  // const checkedSrc = img?.includes("http");
+
+  // if (checkedSrc) return img;
+
+  // const convertImgSrc = `${process.env.NEXT_PUBLIC_API_URL}${img}`;
+
+  // console.log("heeee", img, convertImgSrc);
 
   const defaultImgSrc = defaultImages.userProfileImg;
 
-  const result = img ? convertImgSrc : defaultImgSrc;
+  const result = img ? img : defaultImgSrc;
 
   return result;
 }
