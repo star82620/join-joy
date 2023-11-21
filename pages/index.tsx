e28f7ee01f514fcf3634debae3c139509b0d12f2
Ayu Huang <star82620@gmail.com>
2023-11-21 17:42:50 +0800

    fix/ closed all api of landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,78 +27,85 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   const { req } = context;
   const { authToken } = req.cookies;
 
-  let citiesData = [];
-  let commentsData = [];
-  let nearbyGroupsData = [];
-  let remainingGroupsData = [];
-  let preferenceData = [];
-  let nearbyStoresData = [];
+  let citiesData = defaultCitiesData;
+  let commentsData = defaultCommentsData;
+  let nearbyGroupsData = defaultGroupsData;
+  let remainingGroupsData = defaultGroupsData;
+  let preferenceData = defaultGroupsData;
+  let nearbyStoresData = defaultStoresData;
+
+  // citiesData: defaultCitiesData,
+  // commentsData: defaultCommentsData,
+  // nearbyGroupsData: defaultGroupsData,
+  // remainingGroupsData: defaultGroupsData,
+  // preferenceData: defaultGroupsData,
+  // nearbyStoresData: defaultStoresData,
 
   try {
     const nearbyCityId = 15;
 
     // 取得所有城市
-    const citiesApiParams: apiParamsType = {
-      apiPath: apiPaths.getCities,
-      method: "GET",
-    };
-    const citiesRes = await fetchApi(citiesApiParams);
-    citiesData = citiesRes?.data ?? [];
+    // const citiesApiParams: apiParamsType = {
+    //   apiPath: apiPaths.getCities,
+    //   method: "GET",
+    // };
+    // const citiesRes = await fetchApi(citiesApiParams);
+    // citiesData = citiesRes?.data ?? [];
 
     // 取得最新評價
-    const commentsApiParams: apiParamsType = {
-      apiPath: apiPaths["get-newest-rating"],
-      method: "GET",
-    };
-    const commentsRes = await fetchApi(commentsApiParams);
-    commentsData = commentsRes?.data ?? [];
+    // const commentsApiParams: apiParamsType = {
+    //   apiPath: apiPaths["get-newest-rating"],
+    //   method: "GET",
+    // };
+    // const commentsRes = await fetchApi(commentsApiParams);
+    // commentsData = commentsRes?.data ?? [];
 
     // 有登入用戶的喜好設定
-    const preferenceApiParams: apiParamsType = {
-      apiPath: apiPaths["get-preference-group"],
-      method: "POST",
-      authToken: authToken,
-      data: {
-        page: 1,
-        pageSize: 8,
-      },
-    };
+    // const preferenceApiParams: apiParamsType = {
+    //   apiPath: apiPaths["get-preference-group"],
+    //   method: "POST",
+    //   authToken: authToken,
+    //   data: {
+    //     page: 1,
+    //     pageSize: 8,
+    //   },
+    // };
 
-    const preferenceRes = await fetchApi(preferenceApiParams);
+    // const preferenceRes = await fetchApi(preferenceApiParams);
 
-    if (preferenceRes.status) {
-      preferenceData = preferenceRes.data;
-    }
+    // if (preferenceRes.status) {
+    //   preferenceData = preferenceRes.data;
+    // }
 
     // group IP位置（先固定高雄）
-    const searchNearbyGroups = {
-      ...defaultGroupsSearchKey,
-      cityId: nearbyCityId,
-      page: 1,
-      pageSize: 8,
-    };
+    // const searchNearbyGroups = {
+    //   ...defaultGroupsSearchKey,
+    //   cityId: nearbyCityId,
+    //   page: 1,
+    //   pageSize: 8,
+    // };
 
-    nearbyGroupsData = await getSearchGroups(searchNearbyGroups);
+    // nearbyGroupsData = await getSearchGroups(searchNearbyGroups);
 
     // group 差你一個
-    const searchRemaining = {
-      ...defaultGroupsSearchKey,
-      joinppl: 1,
-      page: 1,
-      pageSize: 8,
-    };
+    // const searchRemaining = {
+    //   ...defaultGroupsSearchKey,
+    //   joinppl: 1,
+    //   page: 1,
+    //   pageSize: 8,
+    // };
 
-    remainingGroupsData = await getSearchGroups(searchRemaining);
+    // remainingGroupsData = await getSearchGroups(searchRemaining);
 
     // 你附近的店家（IP 先定高雄）
-    const searchLocationStores = {
-      ...defaultStoresSearchKey,
-      cityId: nearbyCityId,
-      page: 1,
-      pageSize: 6,
-    };
+    // const searchLocationStores = {
+    //   ...defaultStoresSearchKey,
+    //   cityId: nearbyCityId,
+    //   page: 1,
+    //   pageSize: 6,
+    // };
 
-    nearbyStoresData = await getSearchStores(searchLocationStores);
+    // nearbyStoresData = await getSearchStores(searchLocationStores);
   } catch (error) {
     console.error(error);
   }
