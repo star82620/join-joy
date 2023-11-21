09950f56317566cac6823ac1fc229ca75e6f5000
Ayu Huang <star82620@gmail.com>
2023-11-22 02:32:04 +0800

    update/ adjust CommentCard

--- modules/GroupProfile/CommentsBoard/CommentCard.tsx
+++ modules/GroupProfile/CommentsBoard/CommentCard.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import ProfileImg from "@/common/components/ProfileImg";
 import { CommentCardProps } from "../data";
+import Link from "@/common/components/GeneralLink";
 
 export default function CommentCard({ comment }: CommentCardProps) {
-  const { userName, commentContent, commentDate } = comment;
+  const { userName, commentContent, commentDate, userPhoto, userId } = comment;
 
   return (
-    <div className="flex items-start gap-8">
-      <ProfileImg
-        src="/images/photo-user-000.png"
-        alt="user"
-        sizeStyle="w-16 md:w-9 h-16 md:h-9"
-      />
+    <div className="flex items-start gap-10">
+      <Link href={`/user/${userId}`}>
+        <ProfileImg
+          src={userPhoto}
+          alt={userName}
+          sizeStyle="w-16 md:w-9 h-16 md:h-9"
+        />
+      </Link>
       <div className="grow">
         <div>
           <p className="font-semibold leading-[1.2] md:text-sm md:leading-6 ">
             {userName}
           </p>
-          <p className="text-gray-400 text-xs after:content-['前'] after:ml-0.5">
+          <p className="text-gray-400 text-xs mt-1 after:content-[''] after:ml-0.5">
             {commentDate}
           </p>
         </div>
--- modules/GroupProfile/CommentsBoard/index.tsx
+++ modules/GroupProfile/CommentsBoard/index.tsx
@@ -5,8 +5,36 @@ import fetchApi, { apiParamsType } from "@/common/helpers/fetchApi";
 import CommentCard from "./CommentCard";
 import { GroupDataContext } from "../index";
 import { CommentsDataItemType } from "../data";
+import { AuthContext } from "@/common/contexts/AuthProvider";
 
-const authData = null;
+const CommentTextArea = ({
+  textLength,
+  commentValue,
+  handleInputValue,
+  handleSubmitComment,
+}) => {
+  return (
+    <div className="flex items-center gap-2">
+      <div className="grow">
+        <form id="submitComment" onSubmit={handleSubmitComment}>
+          <textarea
+            maxLength={100}
+            rows={3}
+            placeholder="輸入你想說的話！"
+            className="px-3 py-2 border-b-2 w-full placeholder:text-start"
+            value={commentValue}
+            onChange={handleInputValue}
+          />
+        </form>
+        <p className="text-right text-xs font-bold mt-1">{textLength}/100</p>
+      </div>
+
+      <Button type="submit" appearance="black" rounded form="submitComment">
+        <span className="text-sm">送出</span>
+      </Button>
+    </div>
+  );
+};
 
 const InputWithoutAuth = () => (
   <p className="text-gray-500">
@@ -15,17 +43,25 @@ const InputWithoutAuth = () => (
 );
 
 export default function CommentsBoard() {
+  const authContext = useContext(AuthContext);
+  const { authData, setAuthData } = authContext;
+
   const { commentsData, groupId } = useContext(GroupDataContext);
   const [textLength, setTextLength] = useState(0);
   const [commentValue, setCommentValue] = useState("");
   const isEmpty = commentsData.length === 0;
 
+  const handleInputValue = (e) => {
+    setCommentValue(e.target.value);
+    setTextLength(e.target.value.length);
+  };
+
   async function handleSubmitComment(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log(commentValue);
+
     const msg = {
       groupId: groupId,
-      commentTxt: "string",
+      commentTxt: commentValue,
     };
 
     const submitMsgKey: apiParamsType = {
@@ -34,41 +70,26 @@ export default function CommentsBoard() {
       data: msg,
     };
 
-    const data = await fetchApi(submitMsgKey);
+    const res = await fetchApi(submitMsgKey);
+    console.log(res);
   }
-  const CommentTextArea = () => {
-    return (
-      <div className="flex items-center gap-2">
-        <div className="grow">
-          <form id="submitComment" onSubmit={handleSubmitComment}>
-            <textarea
-              maxLength={100}
-              rows={3}
-              placeholder="輸入你想說的話！"
-              className="px-3 py-2 border-b-2 w-full placeholder:text-start"
-              onChange={(e) => {
-                setCommentValue(e.target.value);
-                setTextLength(e.target.value.length);
-              }}
-            />
-          </form>
-          <p className="text-right text-xs font-bold mt-1">{textLength}/100</p>
-        </div>
-
-        <Button type="submit" appearance="black" rounded form="submitComment">
-          <span className="text-sm">送出</span>
-        </Button>
-      </div>
-    );
-  };
 
   return (
     <section className="px-12 pt-8 pb-10 md:px-3 md:py-4">
       <div className="pb-6 md:pb-4">
-        {!!authData ? <CommentTextArea /> : <InputWithoutAuth />}
+        {!!authData ? (
+          <CommentTextArea
+            textLength={textLength}
+            commentValue={commentValue}
+            handleInputValue={handleInputValue}
+            handleSubmitComment={handleSubmitComment}
+          />
+        ) : (
+          <InputWithoutAuth />
+        )}
       </div>
 
-      <div className="pt-6 md:pt-4 border-t border-gray-300">
+      <div className="flex flex-col gap-6 pt-6 md:pt-4 border-t border-gray-300">
         {isEmpty && (
           <p className="text-center text-gray-600">目前還沒有留言唷！</p>
         )}
