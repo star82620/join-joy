f2cd355c7f581e55110c9ca92c7ad903e7e81b20
Ayu Huang <star82620@gmail.com>
2023-11-22 01:41:09 +0800

    update/ adjust import

--- pages/group/[id].tsx
+++ pages/group/[id].tsx
@@ -3,28 +3,32 @@ import fetchApi, { apiParamsType } from "@/common/helpers/fetchApi";
 import GroupProfile from "@/modules/GroupProfile";
 import Layout from "@/common/components/Layout";
 import {
-  GroupType,
   GroupDataType,
-  CommentsDataType,
   GroupProfilePageProps,
-  getStaticPropsProps,
 } from "@/modules/GroupProfile/data";
 import { GetServerSidePropsContext } from "next";
+import { CommentDataType } from "@/constants/types/commentDataType";
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const { id } = context.params;
+  // 獲取 id
+  const { id } = context.params as { id: string };
   const { authToken } = context.req.cookies;
 
-  console.log(id);
-
+  // 取得揪團資料
   const groupApiParams: apiParamsType = {
     apiPath: `/group/easydetail/${id}`,
     method: "GET",
   };
 
   const groupRes = await fetchApi(groupApiParams);
-  const groupData: GroupDataType = groupRes.data.groupWithGames;
 
+  // if (!groupRes) {
+  //   return console.log("groupRes,NO");
+  // }
+
+  const groupData: GroupDataType = groupRes.data.groupWithGames || [];
+
+  // 取得留言板內容
   const commentsApiParams: apiParamsType = {
     apiPath: `/group/comments/${id}`,
     method: "GET",
@@ -32,12 +36,16 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
   const commentsRes = await fetchApi(commentsApiParams);
 
-  const commentsData: CommentsDataType[] = Array.isArray(commentsRes.data)
+  // if (commentsRes) {
+  //   return console.log("comment,No");
+  // }
+
+  const commentsData: CommentDataType[] = Array.isArray(commentsRes.data)
     ? commentsRes.data
     : [];
 
   return {
-    props: { groupId: id, groupData, commentsData },
+    props: { groupId: id, groupData: groupData, commentsData: commentsData },
   };
 }
 
