6ea67656b2b572477be99f7266b96f3395047823
Ayu Huang <star82620@gmail.com>
2023-11-22 00:45:56 +0800

    update/ adjust import

--- common/components/FollowingButton.tsx
+++ common/components/FollowingButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "./GeneralButton";
 import Image from "./FillImage";
-import icons from "@/constants/iconsPackage/globalIcons";
+import { globalIcons } from "@/constants/iconsPackage/globalIcons";
 
 type Props = {
   isFollowed: boolean;
@@ -11,11 +11,11 @@ export default function FollowingButton({ isFollowed }: Props) {
   const btnAppearance = isFollowed ? "gray" : "orange";
   const btnText = isFollowed ? "已追蹤" : "追蹤";
   const iconSrc = isFollowed
-    ? icons["followed"].src
-    : icons["not-followed"].src;
+    ? globalIcons["followed"].src
+    : globalIcons["not-followed"].src;
   const iconAlt = isFollowed
-    ? icons["followed"].alt
-    : icons["not-followed"].alt;
+    ? globalIcons["followed"].alt
+    : globalIcons["not-followed"].alt;
   return (
     <Button type="button" appearance={btnAppearance}>
       <span className="flex items-center leading-6 font-semibold text-lg md:text-md md:leading-5">
