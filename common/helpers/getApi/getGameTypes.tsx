aa935855a865ef359b96f5e04ba565a8474fdfae
Ayu Huang <star82620@gmail.com>
2023-11-25 13:57:12 +0800

    update/ set api helper

---
+++ common/helpers/getApi/getGameTypes.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import apiPaths from "@/constants/apiPaths";
+import fetchApi, { apiParamsType } from "../fetchApi";
+
+export async function getGameTypes() {
+  // 取得所有遊戲類別
+  const apiParams: apiParamsType = {
+    apiPath: apiPaths["get-all-game-types"],
+    method: "GET",
+  };
+  const res = await fetchApi(apiParams);
+
+  if (!res.status) return null;
+
+  return res.data;
+}
