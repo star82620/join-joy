b8144382ac7285d82b7b4189c27fe68865ca27a7
Ayu Huang <star82620@gmail.com>
2023-12-07 17:51:47 +0800

    update/ change env path

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "ec2-54-238-108-195.ap-northeast-1.compute.amazonaws.com",
+      },
+      {
+        protocol: "https",
         hostname: "2be5-4-224-16-99.ngrok-free.app",
       },
       {
