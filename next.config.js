367074cd8ca5a978e55edfd0a95540ddca0a65c1
Ayu Huang <star82620@gmail.com>
2024-03-12 17:06:37 +0800

    update: adjust hostname in next.config

--- next.config.js
+++ next.config.js
@@ -5,15 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "ec2-54-238-108-195.ap-northeast-1.compute.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "2be5-4-224-16-99.ngrok-free.app",
-      },
-      {
-        protocol: "http",
-        hostname: "4.224.16.99",
+        hostname: "https://joinjoy-api.rocket-coding.com/",
       },
     ],
   },
