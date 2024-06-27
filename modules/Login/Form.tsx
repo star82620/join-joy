62503b27be807bd3430e5917b32e5f83f439bba1
Ayu Huang <star82620@gmail.com>
2024-06-27 16:51:46 +0800

    update: add login form failed reaction

--- modules/Login/Form.tsx
+++ modules/Login/Form.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Inputs from "./Inputs";
 import Button from "@/common/components/GeneralButton";
 import fetchApi from "@/common/helpers/fetchApi";
@@ -11,9 +11,11 @@ import {
 } from "@/common/components/Form/data";
 import { setCookie } from "@/common/helpers/setCookie";
 import { useRouter } from "next/router";
+import { loadingContext } from "@/pages/_app";
 
 export default function Form({ inputSet, btnSet, apiParams }: FormProps) {
   const router = useRouter();
+  const { setIsLoading } = useContext(loadingContext);
 
   const initializeInputStates = (inputs: InputType[]) => {
     //管理 error 狀態的物件，會根據 inputSet 自動生成，預設 false
@@ -36,6 +38,11 @@ export default function Form({ inputSet, btnSet, apiParams }: FormProps) {
   const [inputErrors, setInputErrors] = useState<InputErrorsType>(errors);
   const [inputValues, setInputValues] = useState<InputValuesType>(values);
   const [showPassword, setShowPassword] = useState<ShowPasswordType>(show);
+  const [isLoginFailed, setIsLoginFailed] = useState(false);
+
+  useEffect(() => {
+    if (isLoginFailed) return setIsLoginFailed(false);
+  }, [inputValues]);
 
   // handle 獲取欄位資料
   const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -85,6 +92,7 @@ export default function Form({ inputSet, btnSet, apiParams }: FormProps) {
   // handle 送出表單
   const handleFormSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setIsLoading(true);
     // 表單驗證，如果 false 就擋掉
     const isVerifyPass = checkValidForm();
     if (!isVerifyPass) return;
@@ -94,7 +102,11 @@ export default function Form({ inputSet, btnSet, apiParams }: FormProps) {
 
     // API打出去，還沒有回覆的時候要給 loading 狀態（redux）
     // 如果傳回 status === false (api回應登入失敗)
-    if (!data.status) return;
+    if (!data.status) {
+      setIsLoginFailed(true);
+      setIsLoading(false);
+      return;
+    }
 
     // 成功登入，儲存 token
     setCookie("authToken", data.jwtToken);
@@ -125,6 +137,9 @@ export default function Form({ inputSet, btnSet, apiParams }: FormProps) {
       >
         {children}
       </Button>
+      {isLoginFailed && (
+        <p className="text-danger text-center">登入失敗，請再次確認</p>
+      )}
     </form>
   );
 }
