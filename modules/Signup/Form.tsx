caa7630947adac19d0574eb03b445f5ebff71539
Ayu Huang <star82620@gmail.com>
2024-07-03 15:00:53 +0800

    update: change Signup Form component file location

---
+++ modules/Signup/Form.tsx
@@ -0,0 +1,151 @@
+import React, { useContext, useState } from "react";
+import Inputs from "@/common/components/Form/Inputs";
+import Button from "@/common/components/GeneralButton";
+import fetchApi from "@/common/helpers/fetchApi";
+import {
+  InputType,
+  InputErrorsType,
+  FormProps,
+  ShowPasswordType,
+  InputValuesType,
+} from "@/common/components/Form/data";
+import { loadingContext } from "@/pages/_app";
+import { useRouter } from "next/router";
+
+export default function Form({ inputSet, btnSet, apiParams }: FormProps) {
+  const router = useRouter();
+  const { setIsLoading } = useContext(loadingContext);
+
+  const initializeInputStates = (inputs: InputType[]) => {
+    //管理 error 狀態的物件，會根據 inputSet 自動生成，預設 false
+    const errors: InputErrorsType = {};
+    //管理「input value」狀態的物件，會根據 inputSet 自動生成，預設空值
+    const values: InputValuesType = {};
+    //管理「顯示密碼」狀態的物件，會根據 inputSet 自動生成，預設 false
+    const show: ShowPasswordType = {};
+
+    inputs.forEach((input: InputType) => {
+      errors[input.inputName] = false;
+      values[input.inputName] = "";
+      show[input.inputName] = input.type === "password" ? false : undefined;
+    });
+
+    return { errors, values, show };
+  };
+
+  const { errors, values, show } = initializeInputStates(inputSet);
+  const [inputErrors, setInputErrors] = useState<InputErrorsType>(errors);
+  const [inputValues, setInputValues] = useState<InputValuesType>(values);
+  const [showPassword, setShowPassword] = useState<ShowPasswordType>(show);
+  const [signFailedMsg, setSignFailedMsg] = useState(null);
+
+  // handle 獲取欄位資料
+  const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInputValues((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  // handle 切換密碼顯示狀態
+  const handleShowPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputName = e.target.dataset.target;
+    if (!inputName) return;
+    setShowPassword((prevState) => ({
+      ...prevState,
+      [inputName]: e.target.checked,
+    }));
+  };
+
+  // 表單驗證
+  const checkValidForm = () => {
+    const errors: InputErrorsType = {};
+
+    inputSet.forEach((input) => {
+      const isRequired = input.required;
+      const isMatch = input.pattern;
+
+      // 必填但是沒有指定驗證的欄位就驗證有沒有填
+      if (isRequired && !isMatch) {
+        errors[input.inputName] = !inputValues[input.inputName] ? true : false;
+      }
+      // 有指定驗證內容就依照驗證內容
+      if (isMatch) {
+        errors[input.inputName] = !isMatch.test(inputValues[input.inputName])
+          ? true
+          : false;
+      }
+      // 確認密碼欄位
+      if (input.inputName === "confirmPassword") {
+        const emptyValue = !inputValues.confirmPassword;
+        const isMatch = inputValues.confirmPassword === inputValues.password;
+        const passwordError = !!errors.password;
+
+        errors.confirmPassword = emptyValue || !isMatch || passwordError;
+      }
+    });
+
+    setInputErrors(errors);
+
+    const isVerifyPass = !Object.values(errors).includes(true);
+    return isVerifyPass;
+  };
+
+  // handle 送出表單
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    // 表單驗證，如果 false 就擋掉
+    const isVerifyPass = checkValidForm();
+    if (!isVerifyPass) return setIsLoading(false);
+    // 打 API
+    apiParams.data = inputValues;
+    const data = await fetchApi(apiParams);
+    // 如果傳回 status === false 就不做後續 loading 動作
+    if (!data.status) {
+      setSignFailedMsg(data.message);
+      setIsLoading(false);
+      return;
+    }
+
+    setIsLoading(false);
+    router.push("/");
+  };
+
+  const {
+    type,
+    children,
+    onClick,
+    isDisabled,
+    appearance,
+    className,
+    rounded,
+  } = btnSet;
+  const btnHandleSelector = type === "button" ? onClick : undefined;
+  return (
+    <form
+      className="flex flex-col gap-6 md:gap-4 w-[380px] md:w-[300px] sm:w-[280px]"
+      onSubmit={handleFormSubmit}
+    >
+      <Inputs
+        inputSet={inputSet}
+        inputErrors={inputErrors}
+        inputValues={inputValues}
+        showPassword={showPassword}
+        handleInputValue={handleInputValue}
+        handleShowPassword={handleShowPassword}
+      />
+      {signFailedMsg && (
+        <p className="text-danger text-center">{signFailedMsg}</p>
+      )}
+      <Button
+        type={type}
+        appearance={appearance}
+        onClick={btnHandleSelector}
+        isDisabled={isDisabled}
+        className={className}
+        rounded={rounded}
+      >
+        {children}
+      </Button>
+    </form>
+  );
+}
